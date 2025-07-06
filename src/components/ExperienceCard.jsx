ae2dedb28ea475b96c7fce88a04896036f4bb5d6
Jonathan Roach <jonathan.roachy@gmail.com>
2025-07-06 19:07:35 -0400

    adjust text wrapping for ios

--- src/components/ExperienceCard.jsx
+++ src/components/ExperienceCard.jsx
@@ -7,7 +7,7 @@ function ExperienceCard(props) {
                         <div className='flex items-center px-2 md:px-6'>
                             <img className='w-12 md:w-16 bg-white rounded-full ring-3 ring-offset-2 ring-white mr-3 block' src={props.logo} alt={`${props.company} logo`} />
                             <div className='text-left ml-2 w-full'>
-                                <h1 className='text-base leading-tight text-pretty md:text-xl lg:text-xl font-semibold text-white sm:pr-2'>{props.company}</h1>
+                                <h1 className='text-base leading-tight text-balance md:text-xl lg:text-xl font-semibold text-white'>{props.company}</h1>
                                 <h2 className='text-xs md:text-sm lg:text-base text-[#d3aef6] font-semibold'>{props.title}</h2>
                                 <p className="lg:hidden text-gray-300 md:text-sm text-xs font-semibold">{props.duration}</p>
                                 <p className="text-gray-300 md:text-sm text-xs font-semibold">{props.description}</p>
