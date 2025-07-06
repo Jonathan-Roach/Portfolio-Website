7cb7890bb4b54e176a3365aa26ea76023bca4941
Jonathan Roach <jonathan.roachy@gmail.com>
2025-07-06 00:16:10 -0400

    bug fix

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Skill(props) {
   return (
     <div className='hover:scale-110 flex flex-col items-center p-2'>
-        <img className="w-fit h-6 xl:h-8 mb-1" src={`skill-icons/${props.name.toLowerCase()}.png`}/>
+        <img className="w-fit max-h-6 xl:max-h-8 mb-1" src={`skill-icons/${props.name.toLowerCase()}.png`}/>
         <p className='text-xs lg:text-sm font-semibold text-gray-300'>{props.name}</p>
     </div>
   )
